503471897f19dedb4335db099adaef5547f9eb5b
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-11-27 18:02:06 +0900

    fix: Didn't implement content description properly. style: Reformatted code.

--- app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
+++ app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
@@ -21,75 +21,68 @@ import barqsoft.footballscores.service.myFetchService;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainScreenFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>
-{
-    private static final long ONE_DAY = 1000 * 60 * 60 * 24;
-    public scoresAdapter mAdapter;
-    public static final int SCORES_LOADER = 0;
-    private String[] fragmentdate = new String[1];
-    private int last_selected_item = -1;
+public class MainScreenFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+  private static final long ONE_DAY = 1000 * 60 * 60 * 24;
+  public scoresAdapter mAdapter;
+  public static final int SCORES_LOADER = 0;
+  private String[] fragmentdate = new String[1];
+  private int last_selected_item = -1;
 
-    public MainScreenFragment()
-    {
-    }
+  public MainScreenFragment() {
+  }
 
-    private void update_scores()
-    {
-        Intent service_start = new Intent(getActivity(), myFetchService.class);
-        getActivity().startService(service_start);
+  private void update_scores() {
+    Intent service_start = new Intent(getActivity(), myFetchService.class);
+    getActivity().startService(service_start);
 
-        Intent alarmIntent = new Intent(getActivity(), myFetchService.AlarmReceiver.class);
-        PendingIntent pendingIntent =
-            PendingIntent.getBroadcast(getActivity(), 0, alarmIntent, PendingIntent.FLAG_ONE_SHOT);
-        AlarmManager alarmManager =
-            (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(
-            AlarmManager.RTC,
-            System.currentTimeMillis(),
-            ONE_DAY,
-            pendingIntent);
-    }
-    public void setFragmentDate(String date)
-    {
-        fragmentdate[0] = date;
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             final Bundle savedInstanceState) {
-        update_scores();
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        final ListView score_list = (ListView) rootView.findViewById(R.id.scores_list);
-        mAdapter = new scoresAdapter(getActivity(),null,0);
-        score_list.setAdapter(mAdapter);
-        getLoaderManager().initLoader(SCORES_LOADER,null,this);
-        mAdapter.detail_match_id = MainActivity.selected_match_id;
-        score_list.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                ViewHolder selected = (ViewHolder) view.getTag();
-                mAdapter.detail_match_id = selected.match_id;
-                MainActivity.selected_match_id = (int) selected.match_id;
-                mAdapter.notifyDataSetChanged();
-            }
-        });
-        score_list.setContentDescription(getActivity().getString(R.string.a11y_scores_list));
-        return rootView;
-    }
+    Intent alarmIntent = new Intent(getActivity(), myFetchService.AlarmReceiver.class);
+    PendingIntent pendingIntent =
+        PendingIntent.getBroadcast(getActivity(), 0, alarmIntent, PendingIntent.FLAG_ONE_SHOT);
+    AlarmManager alarmManager =
+        (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+    alarmManager.setRepeating(
+        AlarmManager.RTC,
+        System.currentTimeMillis(),
+        ONE_DAY,
+        pendingIntent);
+  }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
-    {
-        return new CursorLoader(getActivity(),DatabaseContract.scores_table.buildScoreWithDate(),
-                null,null,fragmentdate,null);
-    }
+  public void setFragmentDate(String date) {
+    fragmentdate[0] = date;
+  }
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
-    {
-        //Log.v(FetchScoreTask.LOG_TAG,"loader finished");
-        //cursor.moveToFirst();
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           final Bundle savedInstanceState) {
+    update_scores();
+    View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+    final ListView score_list = (ListView) rootView.findViewById(R.id.scores_list);
+    mAdapter = new scoresAdapter(getActivity(), null, 0);
+    score_list.setAdapter(mAdapter);
+    getLoaderManager().initLoader(SCORES_LOADER, null, this);
+    mAdapter.detail_match_id = MainActivity.selected_match_id;
+    score_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ViewHolder selected = (ViewHolder) view.getTag();
+        mAdapter.detail_match_id = selected.match_id;
+        MainActivity.selected_match_id = (int) selected.match_id;
+        mAdapter.notifyDataSetChanged();
+      }
+    });
+    return rootView;
+  }
+
+  @Override
+  public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+    return new CursorLoader(getActivity(), DatabaseContract.scores_table.buildScoreWithDate(),
+        null, null, fragmentdate, null);
+  }
+
+  @Override
+  public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+    //Log.v(FetchScoreTask.LOG_TAG,"loader finished");
+    //cursor.moveToFirst();
         /*
         while (!cursor.isAfterLast())
         {
@@ -98,23 +91,21 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
         }
         */
 
-        int i = 0;
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast())
-        {
-            i++;
-            cursor.moveToNext();
-        }
-        //Log.v(FetchScoreTask.LOG_TAG,"Loader query: " + String.valueOf(i));
-        mAdapter.swapCursor(cursor);
-        //mAdapter.notifyDataSetChanged();
+    int i = 0;
+    cursor.moveToFirst();
+    while (!cursor.isAfterLast()) {
+      i++;
+      cursor.moveToNext();
     }
+    //Log.v(FetchScoreTask.LOG_TAG,"Loader query: " + String.valueOf(i));
+    mAdapter.swapCursor(cursor);
+    //mAdapter.notifyDataSetChanged();
+  }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader)
-    {
-        mAdapter.swapCursor(null);
-    }
+  @Override
+  public void onLoaderReset(Loader<Cursor> cursorLoader) {
+    mAdapter.swapCursor(null);
+  }
 
 
 }
